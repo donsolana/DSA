26217c045448ed0594b7bb6801904e547f261adb
Dolu Solana <67486270+donsolana@users.noreply.github.com>
2024-03-31 15:01:11 +0100

    Create palindrome.py
    Finding a Palindrome with the two-pointer technique

---
+++ palindrom.py
@@ -0,0 +1,11 @@
+def check_if_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
